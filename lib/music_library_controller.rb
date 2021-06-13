b61ea5b1e51132af34613f5169c17cd7c609b4ee
DJ Taylor <dtaylor88@gmail.com>
2021-06-13 17:56:26 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -28,6 +28,10 @@ class MusicLibraryController
     case input
     when 'list songs'
       list_songs
+    when 'list artists'
+      list_artists
+    when 'list genres'
+      list_genres
     end 
   end 
 
