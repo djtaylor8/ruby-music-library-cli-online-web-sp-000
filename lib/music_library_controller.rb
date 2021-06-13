9da120ee2e89184bc3ea63edc63de3277f735a93
DJ Taylor <dtaylor88@gmail.com>
2021-06-13 17:48:40 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -57,7 +57,7 @@ class MusicLibraryController
       input = gets.strip
       if genre = Genre.find_by_name(input)
         genre.songs.sort{|a,b| a.name <=> b.name}.each.with_index(1) do |s, i|
-          puts "#{i}. #{s.name} - #{s.genre.name}"
+          puts "#{i}. #{s.artist.name} - #{s.song.name}"
         end 
       end
     end 
